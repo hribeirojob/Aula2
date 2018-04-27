fe3584a9329f8d30268855f2dc5ff00b72cf5510
Hugo Jeferson <hribeiro.job@gmail.com>
2018-04-27 00:23:04 -0300

    Criação do formulario, validação dos campos, e colocar a pagina formulario como pag principal

--- src/pages/formulario/formulario.ts
+++ src/pages/formulario/formulario.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 /**
  * Generated class for the FormularioPage page.
@@ -15,11 +16,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FormularioPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  public FormCadast : FormGroup
+  public Nome: String
+  public Sobrenome: String
+  public Endereco: String
+  public Telefone: Number
+  public CPF: Number
+
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              public FrB: FormBuilder ) 
+                {this.FormCadast = FrB.group
+                  ({ Nome: [null, [Validators.required]],
+                    Sobrenome: [null, [Validators.required]],
+                    Endereco: [null, [Validators.required]],
+                    Telefone: [null,[Validators.required]],
+                    CPF: [null, [Validators.required]]
+                  })
+                }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FormularioPage');
+  }
+
+  show_log(){
+    console.log(this.FormCadast.value)
   }
 
 }
